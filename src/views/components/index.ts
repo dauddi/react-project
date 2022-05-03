5cf4afbb13a50acddabd1d4d6cf38118f523a636
dauddi <kuntaytc@gmail.com>
2022-05-03 16:59:06 +0300

    multi-export files

---
+++ src/views/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Layout } from "./Layout";
+export { default as UsersTable } from "./UserTable";
+export { default as Spinner } from "./Spinner";
+export { default as PostCard } from "./PostCard";
---
+++ src/views/index.ts
@@ -0,0 +1,4 @@
+export { default as Homepage } from "./Homepage";
+export { default as UserPage } from "./UserPage";
+export { default as PostIdPage } from "./PostIdPage";
+export { default as UserPosts } from "./UserPosts";
