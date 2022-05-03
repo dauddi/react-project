29da4d5c6f015d9f50053e8fbe50f2f46baa1cee
dauddi <kuntaytc@gmail.com>
2022-05-03 16:56:07 +0300

    initial app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import UsersTable from "./components/UserTable";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./views/components";
+import { Homepage, UserPage, PostIdPage, UserPosts } from "./views";
 
 function App() {
-	return <UsersTable />;
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path=":username" element={<UserPage />} />
+				<Route path=":username/posts" element={<UserPosts />} />
+				<Route path=":username/:postSlug" element={<PostIdPage />} />
+			</Routes>
+		</Layout>
+	);
 }
 
 export default App;
